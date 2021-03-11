888e76b533fbc9cf6cc482714c44fae9b980765f
Tom Bowyer <tom4310@gmail.com>
2021-03-11 20:39:05 +1000

    More prod stuff

--- internal/api/router.go
+++ internal/api/router.go
@@ -10,19 +10,20 @@ func CreateRouter() chi.Router {
 
 	r.Use(SimpleMiddleware())
 
-	r.Get("/healthz", healthz)
-
-	r.Route("/create_user", func(r chi.Router) {
-		r.Post("/", CreateUser)
-	})
-
-	r.Route("/login", func(r chi.Router) {
-		r.Post("/", Login)
-	})
-
-	r.Route("/", func(r chi.Router) {
+	r.Route("/uacl/", func(r chi.Router) {
 		r.Use(verifyJTW())
 		r.Get("/", FetchItems)
+
+		r.Get("/healthz", healthz)
+
+		r.Route("/create_user", func(r chi.Router) {
+			r.Post("/", CreateUser)
+		})
+
+		r.Route("/login", func(r chi.Router) {
+			r.Post("/", Login)
+		})
+
 	})
 
 	return r
