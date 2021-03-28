643c50c5c7a06268a41e2fc5b31a21a2f0073115
Tom Bowyer <tom4310@gmail.com>
2021-03-28 20:43:25 +1000

    Added delete option

--- internal/api/middlewares.go
+++ internal/api/middlewares.go
@@ -40,7 +40,7 @@ func SimpleMiddleware() func(http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			w.Header().Add("Content-Type", "application/json")
 			w.Header().Set("Access-Control-Allow-Origin", "*") // fixme please
-			w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS")
+			w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE")
 			w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, Access-Control-Request-Headers, Access-Control-Request-Method, Connection, Host, Origin, User-Agent, Referer, Cache-Control, X-header")
 
 			if r.Method == "OPTIONS" {
