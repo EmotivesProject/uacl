92524b1b0d92af901abf05d19e44cb5b1fb8854c
Tom Bowyer <tom4310@gmail.com>
2021-04-29 08:34:16 +1000

    Updated prod deploy

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,26 +1,3 @@
 #!/bin/bash
-# Meant to be ran from root directory
 
-stage="$1"
-build="$2"
-
-if [ "$stage" == 'prod' ]; then
-	if [ "$build" == 'yes' ]; then
-		echo Building prod and running it
-		docker-compose -f docker/uacl/docker-compose.prod.yml up -d --build
-		exit
-	fi
-	echo Running prod
-	docker-compose -f docker/uacl/docker-compose.prod.yml up -d
-	exit
-fi
-
-if [ "$build" == 'yes' ]; then
-	echo Building dev and running it
-	docker-compose -f docker/uacl/docker-compose.yml up -d --build
-	exit
-fi
-
-echo Running dev
-docker-compose -f docker/uacl/docker-compose.yml up -d
-
+docker build -f docker/uacl/Dockerfile.prod -t imthetom/uacl_api .
