ddbf9e77440e5fbb0d9b0b4499882aaff9bba41a
Tom Bowyer <tom4310@gmail.com>
2021-05-08 21:38:09 +1000

    Test

--- go.mod
+++ go.mod
@@ -6,7 +6,8 @@ require (
 	github.com/TomBowyerResearchProject/common/logger v0.0.0-20210502204851-4aaa9f3a3338
 	github.com/TomBowyerResearchProject/common/middlewares v0.0.0-20210428074225-ab75e7bf399a
 	github.com/TomBowyerResearchProject/common/postgres v0.0.0-20210503213359-73feb244bff7
-	github.com/TomBowyerResearchProject/common/response v0.0.0-20210428074225-ab75e7bf399a
+	github.com/TomBowyerResearchProject/common/response v0.0.0-20210508000653-21ba51f2b2ba
+	github.com/TomBowyerResearchProject/common/test v0.0.0-20210508112947-de92a325834d // indirect
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/go-chi/chi v4.1.2+incompatible
 	github.com/stretchr/testify v1.7.0
--- test/testing_helper.go
+++ test/testing_helper.go
@@ -1,19 +1,13 @@
 package test
 
 import (
-	"encoding/json"
-	"io"
-	"io/ioutil"
 	"log"
-	"net/http"
 	"net/http/httptest"
 	"os"
-	"testing"
 	"uacl/internal/api"
 
 	"github.com/TomBowyerResearchProject/common/logger"
 	commonPostgres "github.com/TomBowyerResearchProject/common/postgres"
-	"github.com/TomBowyerResearchProject/common/response"
 )
 
 var TS *httptest.Server
@@ -53,55 +47,4 @@ func TearDownIntegrationTest() {
 	con.Close()
 
 	TS.Close()
-}
-
-func TestRequest(
-	t *testing.T,
-	ts *httptest.Server,
-	method,
-	path string,
-	body io.Reader,
-) (
-	*http.Response, map[string]interface{}, []response.Message,
-) {
-	req, err := http.NewRequest(method, ts.URL+path, body)
-	if err != nil {
-		t.Fatal(err)
-
-		return nil, nil, nil
-	}
-
-	return CompleteTestRequest(t, req)
-}
-
-func CompleteTestRequest(t *testing.T, r *http.Request) (*http.Response, map[string]interface{}, []response.Message) {
-	resp, err := http.DefaultClient.Do(r)
-	if err != nil {
-		t.Fatal(err)
-
-		return nil, nil, nil
-	}
-
-	respBody, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		t.Fatal(err)
-
-		return nil, nil, nil
-	}
-	defer resp.Body.Close()
-
-	var responseObj response.Response
-
-	err = json.Unmarshal(respBody, &responseObj)
-	if err != nil {
-		return resp, nil, responseObj.Message
-	}
-
-	resultMap, ok := responseObj.Result.(map[string]interface{})
-
-	if !ok {
-		return resp, nil, nil
-	}
-
-	return resp, resultMap, responseObj.Message
 }
